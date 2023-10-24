ee312cbd241e36c2603ed120d5801eea437356c9
adama diouf <adamadiouf2017@gmail.com>
2023-10-24 13:59:42 -0500

    foter

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,25 +3,19 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='footer'>
+    <div className='footer'>
       <div className='footer-container section-center'>
-        <p>
-          <h3>Diakhoa Sine</h3>
-          <h3>+221 777062556</h3>
-        </p>
-        <p>
-          <span> Mam Biram Diouf</span>
-        </p>
-        <p>
-          <span>Email:adamadiouf2017@gmail.com</span>
-        </p>
-       
+        
+       <p>Diakhoa Sine</p>
+        <p>+221 777062556</p>
+        <p>Mam Biram Diouf</p>
+        <p>Email: adamadiouf2017@gmail.com</p>
+      </div>
+      <p>&copy; 2023 adamadiouf. Tous droits réservés.</p>
       </div>
 
-      <p className='footer-date'>
-        &copy; {new Date().getFullYear()} adamadiouf. Tous droits réservés.
-      </p>
-    </footer>
+      
+   
   )
 }
 
